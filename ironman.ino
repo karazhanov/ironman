f6c9fb645370a41a706b10ffc9db1fec82efc6df
karazhanov <karazhanov@gmail.com>
2019-09-06 17:55:14 +0300

    do all

--- ironman.ino
+++ ironman.ino
@@ -4,117 +4,97 @@
 #include "KGyro.h"
 #include "UTILS.h"
 
- KButton repulsorButton(2);
+// KButton repulsorButton(2);
  KButton stonesButton(3);
 KButton servoButton(4);
-//KServo servo(10);
+KServo servo(10);
 KGyro gyro;
 
 CRGB stonesColors[] = {CRGB::Red, CRGB::Blue, CRGB::Green, CRGB::Yellow, CRGB::Purple, CRGB::Orange};
-//KRgbLed repulsorLeft(3, CRGB::Black, CRGB::WhiteSmoke);
+
+KRgbLed repulsorLeft(3, CRGB::Black, CRGB::WhiteSmoke);
 KRgbLed repulsorRight(3, CRGB::Black, CRGB::White);
 KRgbLed stones(6, CRGB::Black, stonesColors);
-//KRgbLed arcReactor(6, CRGB::Black, CRGB::WhiteSmoke);
-
-bool repulsorOn = false;
-float onRepulsorAngle = 0xFFFFFF;
+KRgbLed arcReactor(6, CRGB::Black, CRGB::WhiteSmoke);
 
 bool KRgbLed::_needUpdate = false;
 
 void setup() {
   Serial.begin(9600);
 
-  repulsorButton.init();
-  stonesButton.init();
+  // repulsorButton.init();
+   stonesButton.init();
   servoButton.init();
 
-//  servo.init(50, 103);
+  servo.init(50, 103);
 
-//  repulsorLeft.init<5>();
+  repulsorLeft.init<5>();
   repulsorRight.init<9>();
 
   stones.init<11>();
-  stones.turnOn();
-//  arcReactor.init<6>();
+  arcReactor.init<6>();
 
-//  gyro.init();
-//  arcReactor.turnOn();
+  gyro.init();
+  arcReactor.turnOn();
   KRgbLed::updateIfNeed();
 }
 
 void loop() {
-//  checkButton(repulsorButton, repulsorRight);
-//  checkButton(stonesButton, arcReactor);
-//  checkButton(servoButton, stones);
-
-//if(stonesButton.isChange() && stonesButton.isPressed()) {
-//  repulsorLeft.switchState();
-//  repulsorRight.switchState();
-//}
-//
-//if(repulsorButton.isChange() && repulsorButton.isPressed()) {
-//  repulsorLeft.switchState();
-//  repulsorRight.switchState();
-//}
-//
-//if(servoButton.isChange() && servoButton.isPressed()) {
-//  repulsorLeft.switchState();
-//  repulsorRight.switchState();
-//}
-
-//  checkServo();
-//  checkGyro();
-
-
-repulsorRight.switchState();
-stones.switchState();
-
-KRgbLed::updateIfNeed();
-delay(1000);
+ checkServo();
+ checkGyro();
+ checkButton(stonesButton, stones);
+ KRgbLed::updateIfNeed();
 }
 
-inline void checkButton(KButton &b, KRgbLed &l) {
-  if(b.isChange() && b.isPressed()) l.switchState();
-}
+ inline void checkButton(KButton &b, KRgbLed &l) {
+   if(b.isChange() && b.isPressed()) l.switchState();
+ }
 
-//inline void checkServo() {
-//   if(servoButton.isChange() && servoButton.isPressed())
-//     if(servo.isOpen()) servo.close();
-//     else servo.open();
-//}
+inline void checkServo() {
+  if(servoButton.isChange() && servoButton.isPressed())
+    if(servo.isOpen()) servo.close();
+    else servo.open();
+}
 
 inline void checkGyro() {
+  static bool repulsorOn = false;
+  static float onRepulsorAngle = 0xFFFFFF;
   gyro.checkState();
-//  print(F("#GYR:"));
-//  print(gyro.getX(), 2);
-//  print(F(","));
-//  print(gyro.getY(), 2);
+  print(F("#GYR:"));
+  print(gyro.getX(), 2);
+  print(F(","));
+  print(gyro.getY(), 2);
 
   if(gyro.getY() < -60) {
     if(!repulsorOn) {
       print("ON REPULSOR ");
-//      repulsorLeft.turnOn();
+      repulsorLeft.turnOn();
       repulsorRight.turnOn();
-//      stones.turnOff();
+      stones.turnOff();
       repulsorOn = true;
       onRepulsorAngle = gyro.getX();
     } else {
-      // if(gyro.getX() < 50 && gyro.getX() > -50) {
-      //
-      // }
+//      if(gyro.getX() < 50 && gyro.getX() > -50) {
+//        stones.turnOn();
+//        repulsorLeft.turnOff();
+//        repulsorRight.turnOff();
+//      } else {
+//        stones.turnOff();
+//        repulsorLeft.turnOn();
+//        repulsorRight.turnOn();
+//      }
     }
  } else {
    if(repulsorOn) {
-//   if(gyro.getX() < 50 && gyro.getX() > -50) {
      print("OFF REPULSOR ");
-//     repulsorLeft.turnOff();
+     repulsorLeft.turnOff();
      repulsorRight.turnOff();
-//     stones.turnOn();
+     stones.turnOff();
      repulsorOn = false;
      onRepulsorAngle = 0xFFFFFF;
    }
  }
-// print("\n");
+ print("\n");
 }
 // resetFunc();  //call reset
 // void(* resetFunc) (void) = 0;//declare reset function at address 0
